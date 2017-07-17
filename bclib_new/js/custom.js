8c9731f13a5c8e787fa8667df20b6c7b6c04f946
Luke Gaudreau <luke@infosoph.org>
2017-07-17 10:24:12 -0400

    Make staging visually distinct

--- bclib_new/js/custom.js
+++ bclib_new/js/custom.js
@@ -26,7 +26,7 @@
     bindings: {parentCtrl: '<'},
     controller: 'prmLogoAfterController',
     template: '<div class="product-logo product-logo-local" layout="row" id="banner" tabindex="0"  role="banner">' +
-'<a href="https://library.bc.edu"><img class="logo-image" alt="{{::(&apos;nui.header.LogoAlt&apos; | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
+'<a href="https://library.bc.edu"><img class="logo-image" alt="{{::(&apos;nui.header.LogoAlt&apos; | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div><h1>Staging</h1>'
   });
 
 })();
