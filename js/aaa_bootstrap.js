ee175678ca580d2f3a3c5092e950fdbe49924995
Scot Colford <scot.colford@bc.edu>
2023-07-06 15:28:45 -0400

    add Google Tag Manager script
    Also remove stale npm-shrinkwrap.json

--- js/aaa_bootstrap.js
+++ js/aaa_bootstrap.js
@@ -12,10 +12,29 @@ var app = angular.module('viewCustom', [
 
 window.browzine = {
     api: "https://public-api.thirdiron.com/public/v1/libraries/431",
-    apiKey: ${bamboo.API_KEY},
+    apiKey: ${bamboo.API_KEY}
 };
 
 browzine.script = document.createElement("script");
 browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
 document.head.appendChild(browzine.script);
 
+/* Google Tag Manager */
+const gtmId = ${GTM_ID}
+function addGTM(doc) {
+ const newScript = doc.createElement('script')
+ const scriptText = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`
+ newScript.innerHTML = scriptText
+ doc.head.append(newScript)
+  
+ const noscript = doc.createElement('noscript')
+ const noscriptText = `&lt;iframe src="//www.googletagmanager.com/ns.html?id=${gtmId}"
+height="0" width="0" style="display:none;visibility:hidden"&gt;&lt;/iframe&gt;`
+ noscript.innerHTML = noscriptText
+ doc.body.insertBefore(noscript, doc.body.firstChild)
+}
+addGTM(document)
